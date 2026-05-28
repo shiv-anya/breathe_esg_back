db24a95749b50bb6419c4ae2aee0c191cde32158
shiv-anya <sg90883@gmail.com>
2026-05-28 16:50:41 +0530

    deployment setup

--- backend/settings.py
+++ backend/settings.py
@@ -4,7 +4,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'dev-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
